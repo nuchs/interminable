00c51a625770014eaa96cfbb9a2fd0669c884ebe
nuchs <sjbrown@live.co.uk>
2024-05-07 15:19:42 +0100

    Extracted terminal management functionality from harness

--- harness/main.go
+++ harness/main.go
@@ -3,105 +3,40 @@ package main
 import (
 	"fmt"
 	"os"
-	"syscall"
 	"time"
-	"unsafe"
 
 	"github.com/nuchs/interminable"
 )
 
-type WinSize struct {
-	rows, cols, x, y uint16
+func main() {
+	run()
+	time.Sleep(3 * time.Second)
+	fmt.Println("--------------------------------------------------")
+	fmt.Println("Ok I love you bye bye!")
 }
 
-func getTerminalSize(fd uintptr) (WinSize, error) {
-	ws := WinSize{}
-
-	_, _, errno := syscall.Syscall(
-		syscall.SYS_IOCTL,
-		fd,
-		syscall.TIOCGWINSZ,
-		uintptr(unsafe.Pointer(&ws)))
-
-	if errno != 0 {
-		return ws, errno
-	}
-
-	return ws, nil
-}
-
-func getTermios(fd uintptr) (syscall.Termios, error) {
-	var t syscall.Termios
-	_, _, err := syscall.Syscall(
-		syscall.SYS_IOCTL,
-		fd,
-		syscall.TCGETS,
-		uintptr(unsafe.Pointer(&t)))
-
-	if err != 0 {
-		return t, err
-	}
-
-	return t, nil
-}
-
-func setTermios(fd uintptr, term syscall.Termios) error {
-	_, _, err := syscall.Syscall(
-		syscall.SYS_IOCTL,
-		fd,
-		syscall.TCSETS,
-		uintptr(unsafe.Pointer(&term)))
-
-	if err != 0 {
+func run() error {
+	term := interminable.Terminal{}
+	if err := term.Open(os.Stdin.Fd()); err != nil {
+		fmt.Println(err)
 		return err
 	}
+	defer term.Close()
 
-	return nil
-}
+	x, y := ((term.Screen.Width-1)/2)-6, (term.Screen.Height-1)/2
+	term.Screen.SetCell(x, y, 'H')
+	term.Screen.SetCell(x+1, y, 'e')
+	term.Screen.SetCell(x+2, y, 'l')
+	term.Screen.SetCell(x+3, y, 'l')
+	term.Screen.SetCell(x+4, y, 'o')
+	term.Screen.SetCell(x+5, y, ' ')
+	term.Screen.SetCell(x+6, y, 'W')
+	term.Screen.SetCell(x+7, y, 'o')
+	term.Screen.SetCell(x+8, y, 'r')
+	term.Screen.SetCell(x+9, y, 'l')
+	term.Screen.SetCell(x+10, y, 'd')
 
-func setRaw(termios *syscall.Termios) {
-	termios.Iflag &^= syscall.IGNBRK | syscall.BRKINT | syscall.PARMRK | syscall.ISTRIP | syscall.INLCR | syscall.IGNCR | syscall.ICRNL | syscall.IXON
-	termios.Oflag &^= syscall.OPOST
-	termios.Lflag &^= syscall.ECHO | syscall.ECHONL | syscall.ICANON | syscall.ISIG | syscall.IEXTEN
-	termios.Cflag &^= syscall.CSIZE | syscall.PARENB
-	termios.Cflag |= syscall.CS8
-	termios.Cc[syscall.VMIN] = 1
-	termios.Cc[syscall.VTIME] = 0
-}
-
-func main() {
-	size, err := getTerminalSize(os.Stdin.Fd())
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-
-	old, err := getTermios(os.Stdin.Fd())
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
-	termios := old
-	setRaw(&termios)
-	defer setTermios(os.Stdin.Fd(), old)
-
-	screen := interminable.NewScreen(int(size.cols), int(size.rows))
-	x, y := ((screen.Width-1)/2)-6, (screen.Height-1)/2
-	screen.SetCell(x, y, 'H')
-	screen.SetCell(x+1, y, 'e')
-	screen.SetCell(x+2, y, 'l')
-	screen.SetCell(x+3, y, 'l')
-	screen.SetCell(x+4, y, 'o')
-	screen.SetCell(x+5, y, ' ')
-	screen.SetCell(x+6, y, 'W')
-	screen.SetCell(x+7, y, 'o')
-	screen.SetCell(x+8, y, 'r')
-	screen.SetCell(x+9, y, 'l')
-	screen.SetCell(x+10, y, 'd')
-
-	s := screen.Render()
+	s := term.Screen.Render()
 	fmt.Print(s)
-
-	time.Sleep(5 * time.Second)
-	fmt.Println("Ok, I love you, byebye")
+	return nil
 }
---
+++ terminal.go
@@ -0,0 +1,108 @@
+package interminable
+
+import (
+	"fmt"
+	"syscall"
+	"unsafe"
+)
+
+type winSize struct {
+	rows, cols       uint16
+	unused1, unused2 uint16 // these are just here to make the struct the right size for the syscall
+}
+
+type Terminal struct {
+	Screen   Screen
+	original syscall.Termios
+	fd       uintptr
+}
+
+func (t *Terminal) Close() error {
+	if err := setTermios(t.fd, t.original); err != nil {
+		return fmt.Errorf("Failed to restore terminal: %w", err)
+	}
+
+	return nil
+}
+
+func (t *Terminal) Open(fd uintptr) error {
+	var err error
+	t.original, err = getTermios(fd)
+	if err != nil {
+		return fmt.Errorf("Failed to get termios: %w", err)
+	}
+
+	termios := t.original
+	setRaw(&termios)
+	if err := setTermios(fd, termios); err != nil {
+		return fmt.Errorf("Failed to set raw mode: %w", err)
+	}
+
+	t.fd = fd
+	ws, err := getTerminalSize(fd)
+	if err != nil {
+		err := fmt.Errorf("Failed to get terminal size: %w", err)
+		if err2 := t.Close(); err2 != nil {
+			return fmt.Errorf("Multiple failures: %w, %w", err, err2)
+		}
+		return err
+	}
+	t.Screen = NewScreen(int(ws.cols), int(ws.rows))
+
+	return nil
+}
+
+func getTerminalSize(fd uintptr) (winSize, error) {
+	ws := winSize{}
+
+	_, _, err := syscall.Syscall(
+		syscall.SYS_IOCTL,
+		fd,
+		syscall.TIOCGWINSZ,
+		uintptr(unsafe.Pointer(&ws)))
+
+	if err != 0 {
+		return ws, err
+	}
+
+	return ws, nil
+}
+
+func getTermios(fd uintptr) (syscall.Termios, error) {
+	var t syscall.Termios
+	_, _, err := syscall.Syscall(
+		syscall.SYS_IOCTL,
+		fd,
+		syscall.TCGETS,
+		uintptr(unsafe.Pointer(&t)))
+
+	if err != 0 {
+		return t, err
+	}
+
+	return t, nil
+}
+
+func setTermios(fd uintptr, term syscall.Termios) error {
+	_, _, err := syscall.Syscall(
+		syscall.SYS_IOCTL,
+		fd,
+		syscall.TCSETS,
+		uintptr(unsafe.Pointer(&term)))
+
+	if err != 0 {
+		return err
+	}
+
+	return nil
+}
+
+func setRaw(termios *syscall.Termios) {
+	termios.Iflag &^= syscall.IGNBRK | syscall.BRKINT | syscall.PARMRK | syscall.ISTRIP | syscall.INLCR | syscall.IGNCR | syscall.ICRNL | syscall.IXON
+	termios.Oflag &^= syscall.OPOST
+	termios.Lflag &^= syscall.ECHO | syscall.ECHONL | syscall.ICANON | syscall.ISIG | syscall.IEXTEN
+	termios.Cflag &^= syscall.CSIZE | syscall.PARENB
+	termios.Cflag |= syscall.CS8
+	termios.Cc[syscall.VMIN] = 1
+	termios.Cc[syscall.VTIME] = 0
+}
